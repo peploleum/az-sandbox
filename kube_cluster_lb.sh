5ebda6ea5f240c1f5d14a90e732ecec7faf58ba4
peploleum <peploleum@gmail.com>
2018-10-10 16:15:12 +0200

    switch to nginx baremetal lb

--- kube_cluster_lb.sh
+++ kube_cluster_lb.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #loadbalancer controller
 
-kubectl create -f https://github.com/kubernetes/ingress-gce/blob/master/examples/deployment/gce-ingress-controller.yaml
-kubectl get po -l name=glbc
+kubectl create -f lb/mandatory.yaml
+kubectl create -f lb/service-nodeport.yaml
+kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx --watch
